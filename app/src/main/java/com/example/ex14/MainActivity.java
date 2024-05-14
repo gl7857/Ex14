0758851f5177156e18cf2fd8b09c211120a1d767
gl7857 <gl7857@bs.amalnet.k12.il>
2024-05-14 22:48:29 +0300

    Ex14

--- app/src/main/java/com/example/ex14/MainActivity.java
+++ app/src/main/java/com/example/ex14/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intentSecondActivity, REQUEST_CODE);
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
