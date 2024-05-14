faf5e44a30ea066d25cf8b761ab61c48ce97bdc3
gl7857 <gl7857@bs.amalnet.k12.il>
2024-05-14 22:47:34 +0300

    Ex14

--- app/src/main/java/com/example/ex14/second_activity.java
+++ app/src/main/java/com/example/ex14/second_activity.java
@@ -3,6 +3,7 @@ package com.example.ex14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
