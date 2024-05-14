5771d498619d00fe8c6c5d1dc6b99ee7bc065953
gl7857 <gl7857@bs.amalnet.k12.il>
2024-05-14 22:38:03 +0300

    Ex14

---
+++ app/src/main/java/com/example/ex14/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.ex14;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText grade1EditText, grade2EditText, grade3EditText, grade4EditText, grade5EditText;
+    int gradeLang=-1, gradeLitr=-1, gradeHist=-1, gradeCitz=-1, gradeBible=-1, mathGrade=-1, mathLevel=-1, englishGrade=-1, englishLevel=-1, answer=-1, grade1=-1, grade2=-1, grade3=-1, selectedOption=-1;
+    String grade1Text,grade2Text,grade3Text,grade4Text,grade5Text, gradeOpt1Text, gradeOpt2Text, gradeOpt3Text;
+    Intent intentSecondActivity;
+    static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        grade1EditText = findViewById(R.id.grade1);
+        grade2EditText = findViewById(R.id.grade2);
+        grade3EditText = findViewById(R.id.grade3);
+        grade4EditText = findViewById(R.id.grade4);
+        grade5EditText = findViewById(R.id.grade5);
+        intentSecondActivity = new Intent(MainActivity.this, second_activity.class);
+    }
+
+    public void grades() {
+        grade1Text = grade1EditText.getText().toString();
+        grade2Text = grade2EditText.getText().toString();
+        grade3Text = grade3EditText.getText().toString();
+        grade4Text = grade4EditText.getText().toString();
+        grade5Text = grade5EditText.getText().toString();
+
+        if (isValidGrade(grade1Text) && isValidGrade(grade2Text) && isValidGrade(grade3Text) && isValidGrade(grade4Text) && isValidGrade(grade5Text)) {
+            gradeLang = Integer.parseInt(grade1Text);
+            gradeLitr = Integer.parseInt(grade2Text);
+            gradeHist = Integer.parseInt(grade3Text);
+            gradeCitz = Integer.parseInt(grade4Text);
+            gradeBible = Integer.parseInt(grade5Text);
+        } else {
+            Toast.makeText(this, "Invalid grade input. Please enter grades between 0 and 100.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public boolean isValidGrade(String grade) {
+        return !grade.isEmpty() && !(Integer.parseInt(grade) > 100);
+    }
+
+    public void next(View view) {
+        grades();
+        Intent intentSecondActivity = new Intent(MainActivity.this, second_activity.class);
+        intentSecondActivity.putExtra("gradeLang", gradeLang);
+        intentSecondActivity.putExtra("gradeLitr", gradeLitr);
+        intentSecondActivity.putExtra("gradeHist", gradeHist);
+        intentSecondActivity.putExtra("gradeCitz", gradeCitz);
+        intentSecondActivity.putExtra("gradeBible", gradeBible);
+        intentSecondActivity.putExtra("mathGrade", mathGrade);
+        intentSecondActivity.putExtra("mathLevel", mathLevel);
+        intentSecondActivity.putExtra("englishGrade", englishGrade);
+        intentSecondActivity.putExtra("englishLevel", englishLevel);
+        intentSecondActivity.putExtra("selectedOption", selectedOption);
+        intentSecondActivity.putExtra("agbara1", gradeOpt1Text);
+        intentSecondActivity.putExtra("grade1", grade1);
+        intentSecondActivity.putExtra("agbara2", gradeOpt2Text);
+        intentSecondActivity.putExtra("grade2", grade2);
+        intentSecondActivity.putExtra("agbara3", gradeOpt3Text);
+        intentSecondActivity.putExtra("grade3", grade3);
+
+        startActivityForResult(intentSecondActivity, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    // השתמש בנתונים שהועברו מהמסך השני
+                    int agbaraGrade1 = data.getIntExtra("agbaraGrade1", -1);
+                    int agbaraGrade2 = data.getIntExtra("agbaraGrade2", -1);
+                    mathGrade = data.getIntExtra("mathGrade", -1);
+                    mathLevel = data.getIntExtra("mathLevel", -1);
+                    englishGrade = data.getIntExtra("englishGrade", -1);
+                    englishLevel = data.getIntExtra("englishLevel", -1);
+                    selectedOption = data.getIntExtra("selectedOption", 0);
+                    grade1 = data.getIntExtra("agbaraGrade1", -1);
+                    grade2 = data.getIntExtra("agbaraGrade2", -1);
+                    grade3 = data.getIntExtra("agbaraGrade3", -1);
+                    gradeOpt1Text = data.getStringExtra("agbaraName1");
+                    gradeOpt2Text = data.getStringExtra("agbaraName2");
+                    gradeOpt3Text = data.getStringExtra("agbaraName3");
+                    // כאן תוכל להשתמש גם בשאר הנתונים שהועברו
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/ex14/second_activity.java
@@ -0,0 +1,354 @@
+package com.example.ex14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class second_activity extends AppCompatActivity {
+    TextView agbara1, agbara2, agbara3, level1, level2, level3;
+    RadioGroup radioGroup;
+    RadioButton option1, option2, option3;
+    EditText agbaraGrade1, agbaraGrade2, agbaraGrade3, edtMathGrade, edtEnglishGrade, edtEnglishLevel,
+            edtMathLevel;
+    Intent intentToResult, intentBack;
+    int mathGrade=-1, mathLevel, englishGrade, englishLevel, grade1, grade2, grade3, selectedOption=0;
+    String mathGradeStr, mathLevelStr, englishGradeStr, englishLevelStr;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        intentToResult = new Intent(second_activity.this, result.class);
+        initViews();
+        intentBack = getIntent();
+        setScreenParams();
+    }
+
+    public void setScreenParams(){
+        mathGrade = intentBack.getIntExtra("mathGrade", -1);
+        if(mathGrade>-1) {
+            edtMathGrade.setText(String.valueOf(mathGrade));
+        }
+        mathLevel = intentBack.getIntExtra("mathLevel", -1);
+        if(mathLevel>-1){
+            edtMathLevel.setText(String.valueOf(mathLevel));
+        }
+        englishGrade = intentBack.getIntExtra("englishGrade", -1);
+        if(englishGrade>-1) {
+            edtEnglishGrade.setText(String.valueOf(englishGrade));
+        }
+        englishLevel = intentBack.getIntExtra("englishLevel", -1);
+        if(englishLevel>-1){
+            edtEnglishLevel.setText(String.valueOf(englishLevel));
+        }
+        selectedOption = intentBack.getIntExtra("selectedOption", 0);
+        if(selectedOption>0){
+            if(selectedOption==1){
+                option1.setChecked(true);
+                clickedOption1(null);
+                String agbara1Name = intentBack.getStringExtra("agbara1");
+                String agbara2Name = intentBack.getStringExtra("agbara2");
+                grade1 = intentBack.getIntExtra("grade1", -1);
+                grade2 = intentBack.getIntExtra("grade2", -1);
+                if(!agbara1Name.isEmpty()){
+                    agbara1.setText(agbara1Name);
+                }
+                if(!agbara2Name.isEmpty()){
+                    agbara2.setText(agbara2Name);
+                }
+                level1.setText("1");
+                level2.setText("5");
+                if(grade1>-1){
+                    agbaraGrade1.setText(String.valueOf(grade1));
+                }
+                if(grade2>-1){
+                    agbaraGrade2.setText(String.valueOf(grade2));
+                }
+            }
+            if(selectedOption==2){
+                option2.setChecked(true);
+                clickedOption2(null);
+                String agbara1Name = intentBack.getStringExtra("agbara1");
+                String agbara2Name = intentBack.getStringExtra("agbara2");
+                grade1 = intentBack.getIntExtra("grade1", -1);
+                grade2 = intentBack.getIntExtra("grade2", -1);
+                if(!agbara1Name.isEmpty()){
+                    agbara1.setText(agbara1Name);
+                }
+                if(!agbara2Name.isEmpty()){
+                    agbara2.setText(agbara2Name);
+                }
+                level1.setText("5");
+                level2.setText("5");
+                if(grade1>-1){
+                    agbaraGrade1.setText(String.valueOf(grade1));
+                }
+                if(grade2>-1){
+                    agbaraGrade2.setText(String.valueOf(grade2));
+                }
+            }
+            if(selectedOption==3){
+                option3.setChecked(true);
+                clickedOption3(null);
+                String agbara1Name = intentBack.getStringExtra("agbara1");
+                String agbara2Name = intentBack.getStringExtra("agbara2");
+                String agbara3Name = intentBack.getStringExtra("agbara3");
+                grade1 = intentBack.getIntExtra("grade1", -1);
+                grade2 = intentBack.getIntExtra("grade2", -1);
+                grade3 = intentBack.getIntExtra("grade3", -1);
+                if(!agbara1Name.isEmpty()){
+                    agbara1.setText(agbara1Name);
+                }
+                if(!agbara2Name.isEmpty()){
+                    agbara2.setText(agbara2Name);
+                }
+                if(!agbara3Name.isEmpty()){
+                    agbara3.setText(agbara3Name);
+                }
+
+                level1.setText("5");
+                level2.setText("5");
+                level3.setText("5");
+
+                if(grade1>-1){
+                    agbaraGrade1.setText(String.valueOf(grade1));
+                }
+                if(grade2>-1){
+                    agbaraGrade2.setText(String.valueOf(grade2));
+                }
+                if(grade3>-1){
+                    agbaraGrade3.setText(String.valueOf(grade3));
+                }
+            }
+        }
+
+    }
+    public void clearSelection(){
+        agbara1.setVisibility(View.INVISIBLE);
+        agbara2.setVisibility(View.INVISIBLE);
+        agbara3.setVisibility(View.INVISIBLE);
+        agbaraGrade1.setVisibility(View.INVISIBLE);
+        agbaraGrade2.setVisibility(View.INVISIBLE);
+        agbaraGrade3.setVisibility(View.INVISIBLE);
+        level1.setVisibility(View.INVISIBLE);
+        level2.setVisibility(View.INVISIBLE);
+        level3.setVisibility(View.INVISIBLE);
+        agbara1.setText("");
+        agbara2.setText("");
+        agbara3.setText("");
+        level1.setText("");
+        level2.setText("");
+        level3.setText("");
+        agbaraGrade1.setText("");
+        agbaraGrade2.setText("");
+        agbaraGrade3.setText("");
+
+        grade1=0;
+        grade2=0;
+        grade3=0;
+    }
+
+    public void clickedOption1(View view) {
+        clearSelection();
+        selectedOption=1;
+        agbara1.setVisibility(View.VISIBLE);
+        agbara2.setVisibility(View.VISIBLE);
+        agbaraGrade1.setVisibility(View.VISIBLE);
+        agbaraGrade2.setVisibility(View.VISIBLE);
+        level1.setVisibility(View.VISIBLE);
+        level2.setVisibility(View.VISIBLE);
+        agbara1.setText("Intro to Science");
+        agbara2.setText(agbara2.getText().toString());
+        level1.setText("1");
+        level2.setText("5");
+    }
+
+    public void clickedOption2(View view) {
+        clearSelection();
+        selectedOption = 2;
+        agbara1.setVisibility(View.VISIBLE);
+        agbara2.setVisibility(View.VISIBLE);
+        agbaraGrade1.setVisibility(View.VISIBLE);
+        agbaraGrade2.setVisibility(View.VISIBLE);
+        level1.setVisibility(View.VISIBLE);
+        level2.setVisibility(View.VISIBLE);
+        agbara1.setText(agbara1.getText().toString());
+        agbara2.setText(agbara2.getText().toString());
+        level1.setText("5");
+        level2.setText("5");
+    }
+
+
+    public void clickedOption3(View view) {
+        clearSelection();
+        selectedOption=3;
+        agbara1.setVisibility(View.VISIBLE);
+        agbara2.setVisibility(View.VISIBLE);
+        agbara3.setVisibility(View.VISIBLE);
+        agbaraGrade1.setVisibility(View.VISIBLE);
+        agbaraGrade2.setVisibility(View.VISIBLE);
+        agbaraGrade3.setVisibility(View.VISIBLE);
+        level1.setVisibility(View.VISIBLE);
+        level2.setVisibility(View.VISIBLE);
+        level3.setVisibility(View.VISIBLE);
+        agbara1.setText(agbara1.getText().toString());
+        agbara2.setText(agbara2.getText().toString());
+        agbara3.setText(agbara3.getText().toString());
+        level1.setText("5");
+        level2.setText("5");
+        level3.setText("5");
+    }
+
+    private void initViews() {
+        radioGroup = findViewById(R.id.radioGroup);
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option2);
+        option3 = findViewById(R.id.option3);
+        agbara1 = findViewById(R.id.agbara1);
+        agbara2 = findViewById(R.id.agbara2);
+        agbara3 = findViewById(R.id.agbara3);
+        level1 = findViewById(R.id.level1);
+        level2 = findViewById(R.id.level2);
+        level3 = findViewById(R.id.level3);
+        agbaraGrade1 = findViewById(R.id.agbaraGrade1);
+        agbaraGrade2 = findViewById(R.id.agbaraGrade2);
+        agbaraGrade3 = findViewById(R.id.agbaraGrade3);
+        edtMathGrade = findViewById(R.id.mathGrade);
+        edtEnglishGrade = findViewById(R.id.englishGrade);
+        edtEnglishLevel = findViewById(R.id.englishLevel);
+        edtMathLevel = findViewById(R.id.mathLevel);
+    }
+
+    public boolean isValidGrade(String grade) {
+        return !grade.isEmpty() && !(Integer.parseInt(grade) > 100);
+    }
+
+
+    public boolean isValidLevel(String level) {
+        return !level.isEmpty() && !(Integer.parseInt(level) > 100);
+    }
+
+    public void setGrades() {
+        int gradeLang = intentBack.getIntExtra("gradeLang",-1);
+        int gradeLitr = intentBack.getIntExtra("gradeLitr",-1);
+        int gradeHist = intentBack.getIntExtra("gradeHist",-1);
+        int gradeCitz = intentBack.getIntExtra("gradeCitz",-1);
+        int gradeBible = intentBack.getIntExtra("gradeBible",-1);
+        intentToResult.putExtra("gradeLang", gradeLang);
+        intentToResult.putExtra("gradeLitr", gradeLitr);
+        intentToResult.putExtra("gradeHist", gradeHist);
+        intentToResult.putExtra("gradeCitz", gradeCitz);
+        intentToResult.putExtra("gradeBible", gradeBible);
+
+
+
+        if (!isValidLevel(edtEnglishLevel.getText().toString()) ||
+                !isValidLevel(edtMathLevel.getText().toString()) ||
+                !isValidGrade(edtMathGrade.getText().toString()) ||
+                !isValidGrade(edtEnglishGrade.getText().toString())) {
+            Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+            mathGrade = 0;
+            mathLevel = 0;
+            englishGrade = 0;
+            englishLevel = 0;
+        } else {
+            mathGradeStr = edtMathGrade.getText().toString();
+            mathLevelStr = edtMathLevel.getText().toString();
+            englishGradeStr = edtEnglishGrade.getText().toString();
+            englishLevelStr = edtEnglishLevel.getText().toString();
+            mathGrade = Integer.parseInt(mathGradeStr);
+            mathLevel = Integer.parseInt(mathLevelStr);
+            englishGrade = Integer.parseInt(englishGradeStr);
+            englishLevel = Integer.parseInt(englishLevelStr);
+            intentToResult.putExtra("mathGrade", mathGrade);
+            intentToResult.putExtra("mathLevel", mathLevel);
+            intentToResult.putExtra("englishGrade", englishGrade);
+            intentToResult.putExtra("englishLevel", englishLevel);
+            intentToResult.putExtra("selectedOption", selectedOption);
+        }
+        if(selectedOption==1){
+            if (!isValidGrade(agbaraGrade1.getText().toString()) ||
+                    !isValidGrade(agbaraGrade2.getText().toString())) {
+                Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+                grade1= 0;
+                grade2= 0;
+            } else {
+                String agbaraGrade1Str = agbaraGrade1.getText().toString();
+                String agbaraGrade2Str = agbaraGrade2.getText().toString();
+                grade1 = Integer.parseInt(agbaraGrade1Str);
+                grade2 = Integer.parseInt(agbaraGrade2Str);
+                intentToResult.putExtra("agbaraGrade1",grade1 );
+                intentToResult.putExtra("agbaraGrade2", grade2);
+                intentToResult.putExtra("level1", 1);
+                intentToResult.putExtra("level2", 5);
+                intentToResult.putExtra("agbaraName1", "Intro to Science");
+                intentToResult.putExtra("agbaraName2", agbara2.getText().toString());
+            }
+        }else if (selectedOption==2){
+            if (!isValidGrade(agbaraGrade1.getText().toString()) ||
+                    !isValidGrade(agbaraGrade2.getText().toString())) {
+                Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+                grade1= 0;
+                grade2= 0;
+            } else {
+                String agbaraGrade1Str = agbaraGrade1.getText().toString();
+                String agbaraGrade2Str = agbaraGrade2.getText().toString();
+                grade1 = Integer.parseInt(agbaraGrade1Str);
+                grade2 = Integer.parseInt(agbaraGrade2Str);
+                intentToResult.putExtra("agbaraGrade1", grade1);
+                intentToResult.putExtra("agbaraGrade2", grade2);
+                intentToResult.putExtra("level1", 5);
+                intentToResult.putExtra("level2", 5);
+                intentToResult.putExtra("agbaraName1", agbara1.getText().toString());
+                intentToResult.putExtra("agbaraName2", agbara2.getText().toString());
+
+            }
+        }else if (selectedOption==3){
+            if (!isValidGrade(agbaraGrade1.getText().toString()) ||
+                    !isValidGrade(agbaraGrade2.getText().toString())||
+                    !isValidGrade(agbaraGrade3.getText().toString())) {
+                Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+                grade1= 0;
+                grade2= 0;
+                grade3= 0;
+            } else {
+                String agbaraGrade1Str = agbaraGrade1.getText().toString();
+                String agbaraGrade2Str = agbaraGrade2.getText().toString();
+                String agbaraGrade3Str = agbaraGrade3.getText().toString();
+                grade1 = Integer.parseInt(agbaraGrade1Str);
+                grade2 = Integer.parseInt(agbaraGrade2Str);
+                grade3 = Integer.parseInt(agbaraGrade3Str);
+                intentToResult.putExtra("agbaraGrade1", grade1);
+                intentToResult.putExtra("agbaraGrade2", grade2);
+                intentToResult.putExtra("agbaraGrade3", grade3);
+                intentToResult.putExtra("level1", 5);
+                intentToResult.putExtra("level2", 5);
+                intentToResult.putExtra("level3", 5);
+                intentToResult.putExtra("agbaraName1", agbara1.getText().toString());
+                intentToResult.putExtra("agbaraName2", agbara2.getText().toString());
+                intentToResult.putExtra("agbaraName3", agbara3.getText().toString());
+
+            }
+        }
+    }
+
+    public void next(View view){
+        setGrades();
+        startActivity(intentToResult);
+    }
+    public void prev(View view){
+        setGrades();
+        setResult(RESULT_OK, intentToResult);
+        finish();
+    }
+}
